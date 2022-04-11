f305f80b63e0c56900e8f1d8c7532a145d7419d9
juan <juancloudcomputing@protonmail.com>
2022-04-11 20:27:56 +0800

    Add the aliases

---
+++ shortify.plugin.zsh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+# Shortify
+# Creates alias to simplify commands
+
+alias     b='bat -n'
+alias btctl='bluetoothctl'
+alias    cp='cp -iv'
+alias    df='df -h'
+alias     e='emerge'
+alias   gcc='gcc -Wall -Wextra -g'
+alias     g='git'
+alias  info="info --vi-keys"
+alias    la='ls -la --human-readable'
+alias    ll='ls -l --human-readable'
+alias     l='ls --human-readable'
+alias    mv='mv -iv'
+alias   nya='doas'
+alias     p='python'
+alias    rm='trash'
+alias    se='doas emerge'
+alias   ssh='TERM="xterm-256color" ssh'
+alias    sv='doasedit.sh'
+alias     t='task'
+alias     v='nvim'
+alias    vw='nvim -c VimwikiIndex'
