0279dfb8b9fe45d0723eba5a12392c22c1a72632
juan <juancloudcomputing@protonmail.com>
2022-05-12 23:00:32 +0800

    Add functions

--- shortify.plugin.zsh
+++ shortify.plugin.zsh
@@ -25,3 +25,43 @@ alias     v='nvim'
 alias   vim='nvim'
 alias    vw='nvim -c VimwikiIndex'
 alias  ffva='ffmpeg -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device /dev/dri/renderD128'
+
+# use z autojumping
+export ZSHZ_CASE=smart
+export ZSHZ_DATA="$HOME/.cache/z"
+zz () {
+  cd `z -l $* | fzf | awk '{print $2}'`
+}
+alias zc="z -c"
+
+# nnn shortcut
+n ()
+{
+	if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
+		echo "nnn is already running"
+		return
+	fi
+	NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+	PAGER="bat" nnn "$@"
+	if [ -f "$NNN_TMPFILE" ]; then
+		. "$NNN_TMPFILE"
+		rm -f "$NNN_TMPFILE" > /dev/null
+	fi
+}
+
+# get cheat sheet
+cb () {
+  curl -s cht.sh/$* | bat --decorations never
+}
+
+#start mpv detached
+dmpv () {
+  mpv $* &> /dev/null &
+  disown
+}
+
+# run mpd if not running
+ncmpcpp () {
+	pidof mpd > /dev/null || mpd.sh > /dev/null || true
+	command ncmpcpp
+}
