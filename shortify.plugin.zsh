decd2dea0b1cd068259159e1c100018e400b5ab7
juan <juancloudcomputing@protonmail.com>
2022-06-12 21:29:27 +0800

    Formatted code, integrate doasedit

--- shortify.plugin.zsh
+++ shortify.plugin.zsh
@@ -3,40 +3,37 @@
 # Shortify
 # Creates alias to simplify commands
 
-alias     b='bat -n'
+alias b='bat -n'
 alias btctl='bluetoothctl'
-alias    cp='cp -iv'
-alias    df='df -h'
-alias     e='emerge'
-alias     g='git'
-alias  info="info --vi-keys"
-alias    la='ls -la --human-readable'
-alias    ll='ls -l --human-readable'
-alias     l='ls'
-alias    mv='mv -iv'
-alias   nya='doas'
-alias     p='python'
-alias    rm='trash'
-alias    se='doas emerge'
-alias   ssh='TERM="xterm-256color" ssh'
-alias    sv='doasedit.sh'
-alias     t='task'
-alias     v='nvim'
-alias   vim='nvim'
-alias    vw='nvim -c VimwikiIndex'
-alias  ffva='ffmpeg -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device /dev/dri/renderD128'
+alias cp='cp -iv'
+alias df='df -h'
+alias e='emerge'
+alias g='git'
+alias info="info --vi-keys"
+alias la='ls -la --human-readable'
+alias ll='ls -l --human-readable'
+alias l='ls'
+alias mv='mv -iv'
+alias nya='doas'
+alias p='python'
+alias rm='trash'
+alias se='doas emerge'
+alias ssh='TERM="xterm-256color" ssh'
+alias sv='doasedit'
+alias v='nvim'
+alias vim='nvim'
+alias ffva='ffmpeg -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device /dev/dri/renderD128'
 
 # use z autojumping
 export ZSHZ_CASE=smart
 export ZSHZ_DATA="$HOME/.cache/z"
-zz () {
-  cd `z -l $* | fzf | awk '{print $2}'`
+zz() {
+	cd $(z -l $* | fzf | awk '{print $2}')
 }
 alias zc="z -c"
 
 # nnn shortcut
-n ()
-{
+n() {
 	if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
 		echo "nnn is already running"
 		return
@@ -45,23 +42,78 @@ n ()
 	PAGER="bat" nnn "$@"
 	if [ -f "$NNN_TMPFILE" ]; then
 		. "$NNN_TMPFILE"
-		rm -f "$NNN_TMPFILE" > /dev/null
+		rm -f "$NNN_TMPFILE" >/dev/null
 	fi
 }
 
 # get cheat sheet
-cb () {
-  curl -s cht.sh/$* | bat --decorations never
+cb() {
+	curl -s cht.sh/$* | bat --decorations never
 }
 
 #start mpv detached
-dmpv () {
-  mpv $* &> /dev/null &
-  disown
+dmpv() {
+	mpv $* &>/dev/null &
+	disown
 }
 
 # run mpd if not running
-ncmpcpp () {
-	pidof mpd > /dev/null || mpd.sh > /dev/null || true
+ncmpcpp() {
+	pidof mpd >/dev/null || mpd.sh >/dev/null || true
 	command ncmpcpp
+}
+
+# doas edit
+# taken from https://github.com/AN3223/scripts/blob/master/doasedit
+
+doasedit() {
+	help() {
+		cat - >&2 <<EOF
+doasedit - like sudoedit, but for doas
+
+doasedit file...
+
+Every argument will be treated as a file to edit. There's no support for
+passing arguments to doas, so you can only doas root.
+
+This script is SECURITY SENSITIVE! Special care has been taken to correctly
+preserve file attributes. Please exercise CAUTION when modifying AND using
+this script.
+EOF
+	}
+
+	case "$1" in --help | -h)
+		help
+		exit 0
+		;;
+	esac
+
+	export TMPDIR=/dev/shm/
+	trap 'trap - EXIT HUP QUIT TERM INT; rm -f "$tmp" "$tmpcopy"' EXIT HUP QUIT TERM INT
+
+	for file; do
+		case "$file" in -*) file=./"$file" ;; esac
+
+		tmp="$(mktemp)"
+		if [ -f "$file" ] && [ ! -r "$file" ]; then
+			doas cat "$file" >"$tmp"
+		elif [ -r "$file" ]; then
+			cat "$file" >"$tmp"
+		fi
+
+		tmpcopy="$(mktemp)"
+		cat "$tmp" >"$tmpcopy"
+
+		${EDITOR:-vi} "$tmp"
+
+		if cmp -s "$tmp" "$tmpcopy"; then
+			echo 'File unchanged, exiting...'
+		else
+			doas dd if="$tmp" of="$file"
+			echo 'Done, changes written'
+		fi
+
+		shred -u "$tmp" "$tmpcopy"
+	done
+
 }
