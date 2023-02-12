397228b648a88832477692593ee4cd09921fd3aa
juan <juancloudcomputing@protonmail.com>
2023-02-12 13:25:48 +0800

    Remove pager option

--- shortify.plugin.zsh
+++ shortify.plugin.zsh
@@ -33,13 +33,14 @@ zz() {
 alias zc="z -c"
 
 # nnn shortcut
+export NNN_PLUG='v:preview-tui'
 n() {
 	if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
 		echo "nnn is already running"
 		return
 	fi
 	NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
-	PAGER="bat" nnn "$@"
+	nnn -ax -P 'v' "$@"
 	if [ -f "$NNN_TMPFILE" ]; then
 		. "$NNN_TMPFILE"
 		rm -f "$NNN_TMPFILE" >/dev/null
