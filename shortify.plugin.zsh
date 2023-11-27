e02f2e45c5da16b5e163674c65b19c4e6b1ee7e0
Ryan <ryan@alien.gov>
2023-11-27 09:56:26 +0800

    Remove unused, add git related, and use sudo

--- shortify.plugin.zsh
+++ shortify.plugin.zsh
@@ -8,52 +8,52 @@ alias btctl='bluetoothctl'
 alias cp='cp -iv'
 alias df='df -h'
 alias e='emerge'
-alias ffva='ffmpeg -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device /dev/dri/renderD128'
 alias g='git'
 alias info="info --vi-keys"
 alias l='ls'
 alias la='ls -la --human-readable'
 alias ll='ls -l --human-readable'
 alias mv='mv -iv'
-alias nya='doas'
 alias p='python'
 alias rm='trash'
 alias s='kitten ssh'
-alias se='doas emerge'
-alias sv='doasedit'
-alias uwu='doas'
+alias se='sudo emerge'
+alias sv='sudoedit'
 alias v='nvim'
 
 # use z autojumping
 export ZSHZ_CASE=smart
 export ZSHZ_DATA="$HOME/.cache/z"
 zz() {
-	cd $(z -l $* | fzf | awk '{print $2}')
+    cd $(z -l $* | fzf | awk '{print $2}')
 }
 alias zc="z -c"
 
 # nnn shortcut
 export NNN_PLUG='v:preview-tui'
 n() {
-	if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
-		echo "nnn is already running"
-		return
-	fi
-	NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
-	nnn -ax -P 'v' "$@"
-	if [ -f "$NNN_TMPFILE" ]; then
-		. "$NNN_TMPFILE"
-		rm -f "$NNN_TMPFILE" >/dev/null
-	fi
+    if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
+        echo "nnn is already running"
+        return
+    fi
+    NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    nnn -ax -P 'v' "$@"
+    if [ -f "$NNN_TMPFILE" ]; then
+        . "$NNN_TMPFILE"
+        rm -f "$NNN_TMPFILE" >/dev/null
+    fi
 }
 
 # get cheat sheet
 cb() {
-	curl -s cht.sh/$* | bat --decorations never
+    curl -s cht.sh/$* | bat --decorations never
 }
 
-#start mpv detached
-dmpv() {
-	mpv $* &>/dev/null &
-	disown
+# Go to project root, https://blog.sushi.money/entry/20100211/1265879271
+up()
+{
+    cd ./$(git rev-parse --show-cdup)
+    if [ $# = 1 ]; then
+        cd $1
+    fi
 }
